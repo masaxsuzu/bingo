c5a62688fbcab773f7da6b7983ca5ee38eb5e590
masaxsuzu <masaxsuzu+github@gmail.com>
2019-12-01 00:35:37 +0900

    Fix injection error on deployment

--- src/app/services/version.service.ts
+++ src/app/services/version.service.ts
@@ -1,20 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VersionService {
-  major: number;
-  minor: number;
-  patch: number;
-  build: string;
+  public major: number;
+  public minor: number;
+  public patch: number;
+  public build: string;
 
-  constructor(major: number, minor: number, patch: number, build: string) {
+  constructor(
+    @Inject('number') major: number,
+    @Inject('number') minor: number,
+    @Inject('number') patch: number,
+    @Inject('string') build: string) {
+
     this.major = major;
     this.minor = minor;
     this.patch = patch;
     this.build = build;
-    console.log(this.getFullVer());
   }
   getSemVer(): string {
     return `v${this.major}.${this.minor}.${this.patch}`;
