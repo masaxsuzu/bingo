a1c7010f52254fca5fdb0703740c202f5cbc1491
masaxsuzu <masaxsuzu+github@gmail.com>
2020-04-30 01:19:17 +0900

    Show storage

--- src/app/pages/config/config.component.ts
+++ src/app/pages/config/config.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../../services/storage.service';
+
+const STORAGE_KEY = 'github.com/masaxsuzu/bingo/v3';
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.less']
 })
+
 export class ConfigComponent implements OnInit {
 
-  constructor() { }
+  max: number;
+  readonly data: any;
+  constructor(readonly storageService: StorageService) {
+    this.max = 100;
+    this.data = storageService.load(STORAGE_KEY);
+  }
 
   ngOnInit(): void {
   }
