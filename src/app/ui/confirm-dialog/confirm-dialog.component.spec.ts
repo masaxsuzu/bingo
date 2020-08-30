221db36d0fe08b7166e75529617639c3c8188df7
masaxsuzu <masaxsuzu+github@gmail.com>
2020-08-30 15:32:26 +0900

    Add a test case

--- src/app/ui/confirm-dialog/confirm-dialog.component.spec.ts
+++ src/app/ui/confirm-dialog/confirm-dialog.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ConfirmDialogComponent } from './confirm-dialog.component';
 
@@ -18,4 +19,18 @@ describe('ConfirmDialogComponent', () => {
     component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
+
+  it('should be close', () => {
+    fixture = TestBed.createComponent(ConfirmDialogComponent);
+    component = fixture.componentInstance;
+    component.data = {
+      title: 'Danger!',
+      contents: 'Do you really want to reset the result?',
+      class: 'modal'
+    };
+
+    component.closeModal();
+    expect(component.isOpen).toBe(false);
+  });
+
 });
